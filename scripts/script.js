155d746c01c277af096634e61ad49468fdfc5c18
Schegolev Sergey <schegolev.s.n@gmail.com>
2021-10-16 18:19:20 +0300

    add adaptive to map block

---
+++ scripts/script.js
@@ -0,0 +1,67 @@
+
+const buttonPageUp = document.querySelector('.footer__column_content_up');
+
+buttonPageUp.onmouseover = function (event) {
+  event.target.style.fill = '#B5B5B5';
+};
+buttonPageUp.onmouseout = function (event) {
+  event.target.style.fill = '#FEFEFE';
+};
+
+buttonPageUp.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
+const region = document.querySelector('.map__region');
+const pinOne = document.getElementById('pin-1');
+const pinTwo = document.getElementById('pin-2');
+
+const adressPopup = document.querySelector('.map__popup');
+
+/*функция открыттия/закрытия попапа*/
+function openPopup(popup) {
+  popup.classList.toggle('map__popup_opened');
+  window.addEventListener('resize', () => {
+    if (document.documentElement.clientWidth < 640) {
+      popup.classList.remove('map__popup_opened');
+    }
+  });
+}
+
+
+/*открытие попапа с адресом*/
+pinOne.addEventListener('click', () => {
+  openPopup(adressPopup);
+});
+pinTwo.addEventListener('click', () => {
+  openPopup(adressPopup);
+});
+
+
+
+/*изменение контента блока map для тачей*/
+
+
+/*смена цвета региона*/
+// region.onmouseover = function (event) {
+//   let target = event.target;
+//   target.setAttribute('fill', '#6D097A');
+// };
+
+// region.onmouseout = function (event) {
+//   let target = event.target;
+//   target.setAttribute('fill', '#9A34A8');
+// };
+
+// pin.onmouseover = function (event) {
+//   let target = event.target;
+//   target.setAttribute('fill', '#6D097A');
+// };
+
+// pin.onmouseout = function () {
+//   region.setAttribute('fill', '#9A34A8');
+// };
+
